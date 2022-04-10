e34b7d38982e88a55ba28430710bdd2c551abbad
Hansel Calderon <Hanzel_u15@hotmail.com>
2022-04-10 11:39:36 -0600

    proyecto creado

---
+++ my-project/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className='text-5xl'>Hansel Calderon Calvo</h1>
+      <h2 className='text-2xl'> <span className='font-bold'>Carnet:</span> B51323</h2>
+    </div>
+  );
+}
+
+export default App;
